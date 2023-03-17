86a4eedffc5a10b2b9a4bca6410e390fd700f146
VadimCurca <vadim.curca14@gmail.com>
2023-03-17 10:44:22 +0200

    move LeNet model to uitls.models module

---
+++ utils/models.py
@@ -0,0 +1,40 @@
+from torch import nn
+
+class LeNet(nn.Module):
+    def __init__(self, distilation):
+        super().__init__()
+
+        self.distilation = distilation
+
+        self.conv1      = nn.Conv2d(1, 6, kernel_size=5, padding=2) # nn.Sigmoid()
+        self.avgPool1   = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.conv2      = nn.Conv2d(6, 16, kernel_size=5)           # nn.Sigmoid()
+        self.avgPool2   = nn.AvgPool2d(kernel_size=2, stride=2)
+        # nn.Flatten()
+        self.linear1    = nn.Linear(16 * 5 * 5, 120)                # nn.Sigmoid()
+        self.linear2    = nn.Linear(120, 84)                        # nn.Sigmoid()
+
+        self.head = nn.Linear(84, 10)
+        self.head_dist = nn.Linear(84, 10)
+
+    def forward(self, x):
+        Y = self.conv1(x)
+        Y = nn.Sigmoid()(Y)
+        Y = self.avgPool1(Y)
+        Y = self.conv2(Y)
+        Y = nn.Sigmoid()(Y)
+        Y = self.avgPool2(Y)
+        Y = nn.Flatten()(Y)
+        Y = self.linear1(Y)
+        Y = nn.Sigmoid()(Y)
+        Y = self.linear2(Y)
+        Y = nn.Sigmoid()(Y)
+
+
+        if self.distilation:
+            x = self.head(Y), self.head_dist(Y)
+            if not self.training:
+                x = (x[0] + x[1]) / 2
+        else:
+            x = self.head(Y)
+        return x
